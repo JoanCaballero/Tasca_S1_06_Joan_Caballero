a70700f1a33f8c5f3b3adb82b1fd4c724c9e9f6d
JoanCaballero <caballero.portoles.joan@gmail.com>
2023-12-20 09:33:22 +0100

    Commit n1 ex2

--- n2exercici2/src/main/java/org/example/Main.java
+++ n2exercici2/src/main/java/org/example/Main.java
@@ -6,16 +6,11 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        GenericMethods gM = new GenericMethods();
         List<Object> llista = new ArrayList<>();
         llista.add(34.6F);
         llista.add("Hola");
-        ArrayList<Integer> array = new ArrayList<>();
-        array.add(0);
-        array.add(1);
-        llista.add(array);
         llista.add(new Persona("Joan", "Caballero", 24));
         llista.add('c');
-        gM.printGenericMethods(llista);
+        GenericMethods.printGenericMethods(llista);
     }
 }
